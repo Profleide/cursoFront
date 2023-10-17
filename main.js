1a11be8af925c66cfd6766a44d1c99896bdbd3a8
teste <leidesilsoarse@hotmail.com>
2023-10-17 14:48:30 -0300

    Crei o cardapio

---
+++ main.js
@@ -0,0 +1,10 @@
+function mostraNomeEIdade() 
+{
+    var name = document.getElementById('nome').value;
+    var age = document.getElementById('idade').value;
+    var name1 = document.getElementById('nome2').value;
+    var age1 = document.getElementById('idade2').value;
+
+    alert('Meu nome é: ' + name + ' e minha idade é: ' + age);
+    alert('Meu nome é: ' + name1 + ' e minha idade é: ' + age1);
+}
